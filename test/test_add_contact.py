bdd505f72931bf56cdd00562062eb89869ece142
Denis <arb.smirnov@gmail.com>
2020-02-07 22:48:44 +0300

    создан единый application.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from fixture.application2 import Application2
+from fixture.application import Application
 import pytest
 
 @pytest.fixture
 def app(request):
-    fixture = Application2()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 def test_test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact (firstname = "Петр", middlename = "Петрович", lastname = "Петров", nickname = "петрич",
                                              title = "ПЕТООР", company = "Петровкакомпания", address = "ул. Петров", home = "84956521474",
                                              mobile = "+75641257896", work = "89541236545", fax = "78444111", email1 = "petrmail@emilo.com",
                                              email2 = "petr2@emilo.com", email3 = "petrmilo@emilo.ru", homepage = "www.petrpetr.ru", bday = "1",
                                              bmonth = "January", byear = "1990", aday = "2", amonth = "February", ayear = "2015", address2 = "Адрес 2",
                                              phone2 = "2", notes = "что то"))
-    app.logout()
+    app.session.logout()
 
 
