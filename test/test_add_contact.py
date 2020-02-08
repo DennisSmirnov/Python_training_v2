4f71d7e993479996dc8c8f1fbd017689f7f31f5d
Denis <arb.smirnov@gmail.com>
2020-02-08 16:43:46 +0300

    тесты работают по отдельности, сохранение перед настройкой управления сессией

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,13 +2,11 @@
 from model.contact import Contact
 #
 def test_test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact (firstname ="Петр", middlename ="Петрович", lastname ="Петров", nickname ="петрич",
                                 title = "ПЕТООР", company = "Петровкакомпания", address = "ул. Петров", home = "84956521474",
                                 mobile = "+75641257896", work = "89541236545", fax = "78444111", email1 = "petrmail@emilo.com",
                                 email2 = "petr2@emilo.com", email3 = "petrmilo@emilo.ru", homepage = "www.petrpetr.ru", bday = "1",
                                 bmonth = "January", byear = "1990", aday = "2", amonth = "February", ayear = "2015", address2 = "Адрес 2",
                                 phone2 = "2", notes = "что то"))
-    app.session.logout()
 
 
